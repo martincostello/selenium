6a5ccb596d6fceb980b791fe60c2ee166c2224e3
martin-costello <martin.costello@just-eat.com>
2017-06-24 13:42:39 +0100

    Workaround encoding issue
    Workaround https://github.com/dotnet/corefx/issues/11715.

--- dotnet/src/webdriver/Internal/ZipStorer.cs
+++ dotnet/src/webdriver/Internal/ZipStorer.cs
@@ -30,7 +30,13 @@ namespace System.IO.Compression
         private static uint[] crcTable = GenerateCrc32Table();
 
         // Default filename encoder
-        private static Encoding defaultEncoding = Encoding.GetEncoding(437);
+        private static Encoding defaultEncoding =
+#if NETSTANDARD1_5
+        Encoding.GetEncoding(1252) // https://github.com/dotnet/corefx/issues/11715
+#else
+        Encoding.GetEncoding(437)
+#endif
+      ;
 
         // List of files to store
         private List<ZipFileEntry> files = new List<ZipFileEntry>();
@@ -415,13 +421,13 @@ namespace System.IO.Compression
 
         /* DOS Date and time:
             MS-DOS date. The date is a packed value with the following format. Bits Description
-                0-4 Day of the month (1–31)
+                0-4 Day of the month (1ï¿½31)
                 5-8 Month (1 = January, 2 = February, and so on)
                 9-15 Year offset from 1980 (add 1980 to get actual year)
             MS-DOS time. The time is a packed value with the following format. Bits Description
                 0-4 Second divided by 2
-                5-10 Minute (0–59)
-                11-15 Hour (0–23 on a 24-hour clock)
+                5-10 Minute (0ï¿½59)
+                11-15 Hour (0ï¿½23 on a 24-hour clock)
         */
         private static uint DateTimeToDosTime(DateTime dateTime)
         {
